67e5bf942d0d4bf07d2f885c754b4b7499fb609a
John <Johnaldooo@berkeley.edu>
2025-02-18 11:16:03 -0800

    adding your_mom.txt and Homework_1.py

---
+++ Homework_1.py
@@ -0,0 +1,6 @@
+# pwd means print working directory, and it displays the current directory of folder you are currently in. I would use it if I am lost. pwd -P shows the absolute path name of the current directory.
+# cd means change directory and it changes the current directory to another directory by naming the diseared directory. I would use this if I want to get to another directory/folder. 
+# ls displays the contents of the directory. I would use it if I forgot all the things in a directory. ls -t sorts by lastest time of modification. 
+# mkdir means make directory and it makes a directory. I would use it when I want to make a directory. mkdir -e creates directories with encryption inheritance.
+# cp means copy files and it copies files. I would use it when I want to copy files. cp -r copies file hierarchies under the file or directory. 
+# mv means move files, and it moves or renames files. I would use this when I want to move or rename files. mv -f does not prompt you before overwriting an existing file.
